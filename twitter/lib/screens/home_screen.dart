7fcb1956c0d2d6e7580e9b83f72aadee3781c39d
SIMYEONAH <simya0528@naver.com>
2023-11-05 11:39:27 +0900

    home screen done!!

--- twitter/lib/screens/home_screen.dart
+++ twitter/lib/screens/home_screen.dart
@@ -11,7 +11,7 @@ class HomeScreen extends StatelessWidget {
       body: SafeArea(
         child: Padding(
           padding: const EdgeInsets.symmetric(
-            horizontal: 20,
+            horizontal: 30,
           ),
           child: SingleChildScrollView(
             child: Column(
@@ -25,7 +25,7 @@ class HomeScreen extends StatelessWidget {
                   size: 40,
                 ),
                 const SizedBox(
-                  height: 170,
+                  height: 160,
                 ),
                 const Text(
                   "See what's happening in the world right now.",
@@ -35,63 +35,147 @@ class HomeScreen extends StatelessWidget {
                   ),
                 ),
                 const SizedBox(
-                  height: 170,
+                  height: 160,
+                ),
+                Container(
+                  decoration: BoxDecoration(
+                      border: Border.all(width: 1, color: Colors.black38),
+                      borderRadius: BorderRadius.circular(30)),
+                  child: CupertinoButton(
+                    color: const Color(0xFFF5F8FA),
+                    borderRadius: BorderRadius.circular(30),
+                    onPressed: () {},
+                    child: const Stack(
+                      alignment: Alignment.center,
+                      children: [
+                        Align(
+                          alignment: Alignment.centerLeft,
+                          child: Icon(
+                            FontAwesomeIcons.google,
+                            color: Colors.black,
+                          ),
+                        ),
+                        Text(
+                          "Continue with Google",
+                          style: TextStyle(
+                            color: Colors.black,
+                            fontWeight: FontWeight.w800,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                const SizedBox(
+                  height: 15,
+                ),
+                Container(
+                  decoration: BoxDecoration(
+                      border: Border.all(width: 1, color: Colors.black38),
+                      borderRadius: BorderRadius.circular(30)),
+                  child: CupertinoButton(
+                    color: const Color(0xFFF5F8FA),
+                    borderRadius: BorderRadius.circular(30),
+                    onPressed: () {},
+                    child: const Stack(
+                      alignment: Alignment.center,
+                      children: [
+                        Align(
+                          alignment: Alignment.centerLeft,
+                          child: Icon(
+                            FontAwesomeIcons.apple,
+                            size: 28,
+                            color: Colors.black,
+                          ),
+                        ),
+                        Text(
+                          "Continue with Apple",
+                          style: TextStyle(
+                            color: Colors.black,
+                            fontWeight: FontWeight.w800,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Container(
+                      height: 1.0,
+                      width: 150.0,
+                      color: Colors.grey[350],
+                    ),
+                    const Text("    or    "),
+                    Container(
+                      height: 1.0,
+                      width: 150.0,
+                      color: Colors.grey[350],
+                    ),
+                  ],
+                ),
+                const SizedBox(
+                  height: 20,
                 ),
                 CupertinoButton(
-                  color: const Color(0xFFF5F8FA),
+                  color: const Color(0xFF14171A),
                   borderRadius: BorderRadius.circular(30),
                   onPressed: () {},
                   child: const Stack(
                     alignment: Alignment.center,
                     children: [
                       Align(
-                        alignment: Alignment.centerLeft,
-                        child: Icon(
-                          FontAwesomeIcons.google,
-                          color: Colors.black,
-                        ),
-                      ),
-                      Text(
-                        "Continue with Google",
-                        style: TextStyle(
-                          color: Colors.black,
-                          fontWeight: FontWeight.w800,
+                        alignment: Alignment.center,
+                        child: Text(
+                          "Create Account",
+                          style: TextStyle(
+                            color: Color(0xFFF5F8FA),
+                            fontSize: 20,
+                            fontWeight: FontWeight.w900,
+                          ),
                         ),
                       ),
                     ],
                   ),
                 ),
                 const SizedBox(
-                  height: 15,
+                  height: 20,
                 ),
-                CupertinoButton(
-                  color: const Color(0xFFF5F8FA),
-                  borderRadius: BorderRadius.circular(30),
-                  onPressed: () {},
-                  child: const Stack(
-                    alignment: Alignment.center,
-                    children: [
-                      Align(
-                        alignment: Alignment.centerLeft,
-                        child: Icon(
-                          FontAwesomeIcons.apple,
-                          size: 28,
-                          color: Colors.black,
-                        ),
-                      ),
-                      Text(
-                        "Continue with Apple",
-                        style: TextStyle(
-                          color: Colors.black,
-                          fontWeight: FontWeight.w800,
-                        ),
-                      ),
-                    ],
+                const Text(
+                  "By signing up, you agree to our Terms, Privacy Policy, and Cookie Use.",
+                  style: TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.w400,
                   ),
                 ),
               ],
             ),
           ),
+        ),
+      ),
+      bottomNavigationBar: BottomAppBar(
+        elevation: 0,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text(
+              "Have an account already?",
+              style: TextStyle(
+                fontSize: 15,
+              ),
+            ),
+            TextButton(
+              onPressed: () {},
+              child: const Text(
+                "Log in",
+                style: TextStyle(fontSize: 15),
+              ),
+            ),
+          ],
         ),
       ),
     );
