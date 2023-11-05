3c57c8542af69c3552e962ffa745a8a2deefac41
SIMYEONAH <simya0528@naver.com>
2023-11-05 17:02:59 +0900

    Verify Screen Done!!!

---
+++ twitter/lib/screens/password_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class PasswordScreen extends StatefulWidget {
+  const PasswordScreen({super.key});
+
+  @override
+  State<PasswordScreen> createState() => _PasswordScreenState();
+}
+
+class _PasswordScreenState extends State<PasswordScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
--- twitter/lib/screens/verify_screen.dart
+++ twitter/lib/screens/verify_screen.dart
@@ -1,15 +1,311 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:twitter/screens/password_screen.dart';
 
 class VerifyScreen extends StatefulWidget {
-  const VerifyScreen({super.key});
+  const VerifyScreen({
+    super.key,
+  });
 
   @override
   State<VerifyScreen> createState() => _VerifyScreenState();
 }
 
 class _VerifyScreenState extends State<VerifyScreen> {
+  final TextEditingController _VerifyController1 = TextEditingController();
+  final TextEditingController _VerifyController2 = TextEditingController();
+  final TextEditingController _VerifyController3 = TextEditingController();
+  final TextEditingController _VerifyController4 = TextEditingController();
+
+  final _formkey = GlobalKey<FormState>();
+  List codeList = [];
+
+  @override
+  initState() {
+    super.initState();
+    _VerifyController1.addListener(() {});
+    _VerifyController2.addListener(() {});
+    _VerifyController3.addListener(() {});
+    _VerifyController4.addListener(() {});
+    setState(() {});
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+    _VerifyController1.dispose();
+    _VerifyController2.dispose();
+    _VerifyController3.dispose();
+    _VerifyController4.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return SafeArea(
+      child: GestureDetector(
+        onTap: () {
+          FocusManager.instance.primaryFocus?.unfocus();
+        },
+        child: Scaffold(
+          appBar: AppBar(
+            iconTheme: const IconThemeData(
+              color: Colors.black,
+            ),
+            centerTitle: true,
+            backgroundColor: Colors.transparent,
+            elevation: 0.0,
+            title: Icon(
+              FontAwesomeIcons.twitter,
+              color: Colors.blue[300],
+              size: 40,
+            ),
+          ),
+          body: Padding(
+            padding: const EdgeInsets.symmetric(
+              horizontal: 20,
+            ),
+            child: SingleChildScrollView(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const SizedBox(
+                    height: 40,
+                  ),
+                  const Text(
+                    "We sent you a code",
+                    style: TextStyle(fontSize: 35, fontWeight: FontWeight.w900),
+                  ),
+                  const SizedBox(
+                    height: 40,
+                  ),
+                  const Text(
+                    "Enter it below to verify",
+                    style: TextStyle(
+                      fontSize: 15,
+                    ),
+                  ),
+                  const Text(
+                    "jhon.mobbin@gmail.com.",
+                    style: TextStyle(
+                      fontSize: 15,
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 40,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Container(
+                        height: MediaQuery.of(context).size.shortestSide * 0.13,
+                        decoration: BoxDecoration(
+                          border: Border.all(color: Colors.grey),
+                          borderRadius: BorderRadius.circular(5),
+                          color: Colors.white,
+                          shape: BoxShape.rectangle,
+                        ),
+                        child: AspectRatio(
+                          aspectRatio: 1,
+                          child: Form(
+                            child: TextFormField(
+                              controller: _VerifyController1,
+                              decoration: const InputDecoration(
+                                border: InputBorder.none,
+                              ),
+                              textAlign: TextAlign.center,
+                              keyboardType: TextInputType.number,
+                              style: const TextStyle(),
+                              obscureText: true,
+                              maxLines: 1,
+                              onChanged: (value) {
+                                if (value.isNotEmpty && value.length == 1) {
+                                  codeList.add(value);
+
+                                  FocusScope.of(context).nextFocus();
+                                }
+                              },
+                              inputFormatters: <TextInputFormatter>[
+                                FilteringTextInputFormatter.digitsOnly
+                              ],
+                            ),
+                          ),
+                        ),
+                      ),
+                      Container(
+                        height: MediaQuery.of(context).size.shortestSide * 0.13,
+                        decoration: BoxDecoration(
+                          border: Border.all(color: Colors.grey),
+                          borderRadius: BorderRadius.circular(5),
+                          color: Colors.white,
+                          shape: BoxShape.rectangle,
+                        ),
+                        child: AspectRatio(
+                          aspectRatio: 1,
+                          child: Form(
+                            child: TextFormField(
+                              controller: _VerifyController2,
+                              decoration: const InputDecoration(
+                                border: InputBorder.none,
+                              ),
+                              textAlign: TextAlign.center,
+                              keyboardType: TextInputType.number,
+                              style: const TextStyle(),
+                              obscureText: true,
+                              maxLines: 1,
+                              onChanged: (value) {
+                                if (value.isNotEmpty && value.length == 1) {
+                                  codeList.add(value);
+
+                                  FocusScope.of(context).nextFocus();
+                                }
+                              },
+                              inputFormatters: <TextInputFormatter>[
+                                FilteringTextInputFormatter.digitsOnly
+                              ],
+                            ),
+                          ),
+                        ),
+                      ),
+                      Container(
+                        height: MediaQuery.of(context).size.shortestSide * 0.13,
+                        decoration: BoxDecoration(
+                          border: Border.all(color: Colors.grey),
+                          borderRadius: BorderRadius.circular(5),
+                          color: Colors.white,
+                          shape: BoxShape.rectangle,
+                        ),
+                        child: AspectRatio(
+                          aspectRatio: 1,
+                          child: Form(
+                            child: TextFormField(
+                              controller: _VerifyController3,
+                              decoration: const InputDecoration(
+                                border: InputBorder.none,
+                              ),
+                              textAlign: TextAlign.center,
+                              keyboardType: TextInputType.number,
+                              style: const TextStyle(),
+                              obscureText: true,
+                              maxLines: 1,
+                              onChanged: (value) {
+                                if (value.isNotEmpty && value.length == 1) {
+                                  codeList.add(value);
+
+                                  FocusScope.of(context).nextFocus();
+                                }
+                              },
+                              inputFormatters: <TextInputFormatter>[
+                                FilteringTextInputFormatter.digitsOnly
+                              ],
+                            ),
+                          ),
+                        ),
+                      ),
+                      Container(
+                        height: MediaQuery.of(context).size.shortestSide * 0.13,
+                        decoration: BoxDecoration(
+                          border: Border.all(color: Colors.grey),
+                          borderRadius: BorderRadius.circular(5),
+                          color: Colors.white,
+                          shape: BoxShape.rectangle,
+                        ),
+                        child: AspectRatio(
+                          aspectRatio: 1,
+                          child: Form(
+                            child: TextFormField(
+                              controller: _VerifyController4,
+                              onSaved: (newValue) {},
+                              decoration: const InputDecoration(
+                                border: InputBorder.none,
+                              ),
+                              textAlign: TextAlign.center,
+                              keyboardType: TextInputType.number,
+                              style: const TextStyle(),
+                              obscureText: true,
+                              maxLines: 1,
+                              onChanged: (value) {
+                                if (value.isNotEmpty && value.length == 1) {
+                                  codeList.add(value);
+                                  FocusScope.of(context).nextFocus();
+                                }
+                              },
+                              inputFormatters: <TextInputFormatter>[
+                                FilteringTextInputFormatter.digitsOnly
+                              ],
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                  const SizedBox(
+                    height: 50,
+                  ),
+                  Text(
+                    "Didn't receive email?",
+                    style: TextStyle(color: Colors.blue[400]),
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      SizedBox(
+                        width: 350,
+                        height: 55,
+                        child: CupertinoButton(
+                          onPressed: () {
+                            if (codeList.length == 4) {
+                              Navigator.of(context).push(
+                                MaterialPageRoute(
+                                  builder: ((context) =>
+                                      const PasswordScreen()),
+                                ),
+                              );
+                            } else {
+                              return;
+                            }
+                          },
+                          borderRadius: BorderRadius.circular(20),
+                          color: Colors.black,
+                          child: const Text(
+                            "Next",
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 20,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                  const SizedBox(
+                    height: 30,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      codeList.length == 4
+                          ? const Icon(
+                              FontAwesomeIcons.circleCheck,
+                              color: Colors.green,
+                            )
+                          : const Icon(
+                              FontAwesomeIcons.circleCheck,
+                              color: Colors.red,
+                            ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
